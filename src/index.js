0ed0a0102446f726de1e67690b600b447730add6
pachacamac <marcanbe@gmail.com>
2024-03-12 23:49:12 +0100

    localstorage

--- src/index.js
+++ src/index.js
@@ -27,14 +27,17 @@ const INDEX_HTML = html`<!DOCTYPE html>
         .fab, .far, .fas { padding: 5px; font-size: 20px; text-align: center; color: #222; vertical-align: middle; }
         body a:not(.highlighted-words) { color: #dcd9d4; }
         #canvas { width: 100vw; height: 75vh; position: absolute; left: 0; bottom: 0; z-index: -1;}
-        textarea { background-color: #222; color: #dcd9d4; }
-        button { background-color: #222; color: #dcd9d4; }
+        textarea,button { background-color: #222; color: #dcd9d4; border-radius:10px;}
         .hidden { display: none; }
         .code { font: 1em 'Fira Code', monospace !important; max-height: 33vh; overflow: auto; border: 1px solid rgba(255,255,255,0.2);}
         .code.shj-multiline.shj-mode-header:before { content: attr(data-language); }
         .code.result { max-height: calc(76px * 4); }
         .code.result.shj-multiline.shj-mode-header:before { content: "⚙ execution result"; cursor: pointer; }
         .code.result:hover, .code.result:active, .code.result:focus { max-height: 50vh; }
+        .task{ white-space: nowrap; overflow: hidden; text-overflow: ellipsis; border: 1px solid rgba(255,255,255,0.2); border-radius:10px; padding: 0.25em; margin-bottom: 0.125em;}
+        .task i{ margin-right: 0.5em; cursor: pointer;}
+        .task i:hover { color: #339FF0; transition: color 0.3s ease; }
+
     </style>
     <link rel="icon" type="image/png" href="favicon.png">
     <meta property="og:title" content="do.new">
@@ -61,13 +64,20 @@ const INDEX_HTML = html`<!DOCTYPE html>
                         <a class="fab fa-linkedin-in" href=""></a>
                         <a class="fab fa-github" href=""></a>
                     </div>
+                    <div id="tasks">
+                    </div>
                 </div>
                 <div class="one-half column" style="margin-top: 25%">
                   <div id="form">
                       <textarea id="inputArea" name="codeRequest" rows="6" cols="50" placeholder="Enter your code request ... e.g. write a greeting function that takes a name and outputs a greeting and call it"></textarea>
                       <button id="submitButton">Submit <i class="fas fa-cog fa-spin hidden" style="color: #dcd9d4"></i></button>
                   </div>
-                  <div id="result" class="hidden"></div>
+                  <div id="result" class="hidden">
+                    <div id="codebox" class="code shj-lang-py"></div>
+                    <p id="exres-msgbox" class="hidden"></p>
+                    <div id="exresbox" class="code shj-mode-header shj-lang-plain result hidden"></div>
+                    <button id="backtoform">Go back</button>
+                  </div>
                 </div>
             </div>
         </div>
@@ -76,21 +86,76 @@ const INDEX_HTML = html`<!DOCTYPE html>
     <!-- <footer><div class="container"><div class="row"></div></div></footer> -->
 <script type="module">
   import { highlightElement } from 'https://unpkg.com/@speed-highlight/core/dist/index.js';
-  
+  const randomId = (len) => [...Array(len)].map(() => Math.random().toString(36)[2] || '0').join('');
+  document.getElementById('backtoform').addEventListener('click', showForm);
+  function updateTaskList(){
+    const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    const taskDiv = document.getElementById('tasks');
+    while (taskDiv.firstChild) taskDiv.removeChild(taskDiv.firstChild);
+    tasks.forEach((t)=>{
+      const e = document.createElement('div');
+      e.classList.add('task');
+      e.id = t.id;
+      e.setAttribute('data-request', t.request);
+      e.setAttribute('data-response', t.response);
+      e.setAttribute('data-result', t.result);
+      e.addEventListener('click', (event) => {event.stopPropagation(); showTask(t.request, t.response, t.result)});
+      const del = document.createElement('i');
+      del.classList.add('fa','fa-trash');
+      del.addEventListener('click', (event) => { event.stopPropagation(); delTask(t.id); });
+      e.appendChild(del);
+      e.appendChild(document.createTextNode(t.request));
+      taskDiv.appendChild(e);
+    });
+  }
+  function addTask(request, response, result){
+    const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    tasks.unshift({id: randomId(), request, response, result});
+    localStorage.setItem('tasks', JSON.stringify(tasks.slice(0,25))); //limit
+    updateTaskList();
+  }
+  function delTask(id){
+    localStorage.setItem('tasks', JSON.stringify(
+      JSON.parse(localStorage.getItem('tasks') || '[]').filter(t=>t.id!=id)
+    ));
+    updateTaskList();
+  }
+  function getTask(id){
+    return JSON.parse(localStorage.getItem('tasks') || '[]').find(t=>t.id==id)
+  }
+  function showTask(request, response, result){
+    const codebox = document.getElementById('codebox');
+    codebox.textContent = response;
+    highlightElement(codebox);
+    codebox.classList.remove('hidden');
+    document.getElementById('exres-msgbox').classList.add('hidden');
+    const exresbox = document.getElementById('exresbox');
+    exresbox.textContent = result;
+    highlightElement(exresbox);
+    exresbox.classList.remove('hidden');
+    document.getElementById('result').classList.remove('hidden');
+    document.getElementById('form').classList.add('hidden');
+  }
+  function showForm(){
+    document.getElementById('result').classList.add('hidden');
+    document.getElementById('codebox').textContent = '';
+    document.getElementById('exresbox').textContent = '';
+    document.getElementById('form').classList.remove('hidden');
+    submitBtn.querySelector('.hidden').classList.toggle('hidden');
+  }
+  updateTaskList();
+
   const submitBtn = document.getElementById('submitButton');
   submitBtn.addEventListener('click', async function() {
     const userRequest = document.getElementById('inputArea').value;
     submitBtn.disabled = true;
     submitBtn.querySelector('.hidden').classList.toggle('hidden');
-    
     const resultDiv = document.getElementById('result');
     const formDiv = document.getElementById('form');
-    resultDiv.classList.toggle('hidden');
-    formDiv.classList.toggle('hidden');
-    const codeBox = document.createElement('div');
-    codeBox.classList.add('code', 'shj-lang-py');
-    codeBox.textContent = '"""'+userRequest+'"""\\n';
-    resultDiv.appendChild(codeBox);
+    formDiv.classList.add('hidden');
+    resultDiv.classList.remove('hidden');
+    const codebox = document.getElementById('codebox');
+    codebox.textContent = '"""'+userRequest+'"""\\n';
     const codeRequest = document.getElementById('inputArea').value;
     const source = new EventSource('/?q='+encodeURIComponent(codeRequest));
     source.onerror = (e) => {console.error('onerror',e); source.close();}
@@ -98,27 +163,26 @@ const INDEX_HTML = html`<!DOCTYPE html>
       // console.log(event.data);
       try {
         const data = JSON.parse(event.data);
-        codeBox.textContent += data.response;
+        codebox.textContent += data.response;
         //console.log(data);
         if (data.isFinal || data.response === undefined) {
           source.close();
           const code = data.codeBlocks?.[0]?.code;
           if(!code) return;
-          codeBox.textContent = '"""'+data.userRequest+'"""\\n'+code;
-          highlightElement(codeBox);
-          const msgBox = document.createElement('p');
-          msgBox.id = 'exres-msgbox';
+          codebox.textContent = '"""'+data.userRequest+'"""\\n'+code;
+          highlightElement(codebox);
+          const msgBox = document.getElementById('exres-msgbox');
           msgBox.innerHTML = "Hold on, we're executing your code ... <i class='fas fa-cog fa-spin' style='color: #dcd9d4'></i>";
-          resultDiv.appendChild(msgBox);
+          msgBox.classList.remove('hidden');
           const pyKernel = new PyodideKernel();
           await pyKernel.init();
-          document.getElementById('exres-msgbox').remove();
-          const exresBox = document.createElement('div');
-          exresBox.classList.add('code', 'shj-mode-header', 'shj-lang-plain', 'result');
-          resultDiv.appendChild(exresBox);
-          pyKernel.run(code, {stdoutCB: t=>{exresBox.textContent+=t}, stderrCB: t=>{exresBox.textContent+=t}}).then(exres=>{
-            if(exres.result!==undefined) exresBox.textContent+="\\n>> "+exres.result;
-            highlightElement(exresBox);
+          document.getElementById('exres-msgbox').classList.add('hidden');
+          const exresbox = document.getElementById('exresbox');
+          exresbox.classList.remove('hidden');
+          pyKernel.run(code, {stdoutCB: t=>{exresbox.textContent+=t}, stderrCB: t=>{exresbox.textContent+=t}}).then(exres=>{
+            if(exres.result!==undefined) exresbox.textContent+="\\n>> "+exres.result;
+            addTask(data.userRequest, codebox.textContent, exresbox.textContent);
+            highlightElement(exresbox);
           });
           return;
         }
